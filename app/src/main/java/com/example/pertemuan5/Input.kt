9d9442b04f1e52ca36957c1cd3406686631c8498
Jaidil14 <jaidilrachmad@gmail.com>
2025-10-30 12:00:44 +0700

    Menambahkan komponen kolom serta modifiernya

--- app/src/main/java/com/example/pertemuan5/Input.kt
+++ app/src/main/java/com/example/pertemuan5/Input.kt
@@ -1,8 +1,10 @@
 package com.example.pertemuan5
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
@@ -16,4 +18,19 @@ fun FormDataDiri(modifier: Modifier) {
     var jenis by remember { mutableStateOf("") }
 
     val gender:List<String> = listOf("Laki-laki", "Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textNama,
+            singleLine = true,
+            shape = MaterialTheme.shapes.large,
+            modifier = Modifier.width(250.dp),
+            label = { Text(text = " Nama Lengkap") },
+            onValueChange = {
+                textNama = it
+            }
+        )
+    }
 }
