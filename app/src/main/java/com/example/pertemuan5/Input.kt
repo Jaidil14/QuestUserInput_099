06eaa35c419ec11b0f16c96b36331c88a53bd869
Jaidil14 <jaidilrachmad@gmail.com>
2025-10-30 11:37:37 +0700

    Menambahkan fungsi serta deklarasi 3 buah variabel untuk mengingat nilai input dari keyboard

--- app/src/main/java/com/example/pertemuan5/Input.kt
+++ app/src/main/java/com/example/pertemuan5/Input.kt
@@ -1,2 +1,13 @@
 package com.example.pertemuan5
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FormDataDiri(modifier: Modifier) {
+    var textNama by remember { mutableStateOf(value = "") }
+    var textAlamat by remember { mutableStateOf(value = "") }
+    var textJK by remember { mutableStateOf(value = "") }
+}
